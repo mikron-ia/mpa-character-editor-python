3bf87eaf65b10db60fa93d558e569b1503df25cb
Mikron <mikronia@gmail.com>
2018-09-04 19:04:01 +0200

    Tests for Cost constructor
    Concerns #1

--- tests/test_cost.py
+++ tests/test_cost.py
@@ -15,7 +15,17 @@ class TestCost(TestBase):
     def test_fibonacci(self):
         self.assertEqual([1, 2, 3, 5, 8], Cost.generate_progress_fibonacci(5))
 
-    def test_sum_creation(self):
+    def test_sum_creation_standard(self):
         progress = [1, 2, 3, 4, 5, 6, 7]
         result = [1, 3, 6, 10, 15, 21, 28]
         self.assertEqual(result, Cost.create_cost_by_level(progress))
+
+    def test_sum_creation_unusual(self):
+        progress = [1, -1, 1, -1, 1, -1, 1]
+        result = [1, 0, 1, 0, 1, 0, 1]
+        self.assertEqual(result, Cost.create_cost_by_level(progress))
+
+    def test_object_creation(self):
+        cost = Cost('linear', 5)
+        self.assertEqual([1, 2, 3, 4, 5], cost.of_level)
+        self.assertEqual([1, 3, 6, 10, 15], cost.total_level)
