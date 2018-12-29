d26aab2c40367599e8eef50e7f0c41dae577d140
Mikron <mikronia@gmail.com>
2018-12-29 12:58:46 +0100

    Main editor reworking has started

--- editor.py
+++ editor.py
@@ -1,4 +1,8 @@
 import urwid
+import sys
+import json
+from domain.errors import ConfigError
+from domain.character import Character
 
 
 def exit_on_quit(key):
@@ -6,11 +10,40 @@ def exit_on_quit(key):
         raise urwid.ExitMainLoop()
 
 
-if __name__ == "__main__":
-    title = u" MPA Character Editor"
-    txt = urwid.Text(('banner', title), align='center')
-    map1 = urwid.AttrMap(txt, 'streak')
+def prepare_config_file():
+    if len(sys.argv) < 2:
+        raise ConfigError('Provide config file name as the first parameter.')
+
+    config_file_name = './config/' + str(sys.argv[1]).rstrip('.json') + '.json'
+    config_file = open(config_file_name, 'r')
+
+    contents = config_file.read()
+    configuration = json.loads(contents)
+
+    if not configuration:
+        raise ConfigError('File not found')
+
+    return configuration
+
+
+def main_loop():
+    configuration = prepare_config_file()
+    character = Character(configuration, dict())
+
+    # todo: Config tally - how many skills, how does it look, etc.
+
+    title = 'MPA Character Editor'
+    header = urwid.Text(('banner', title), align='center')
+    map1 = urwid.AttrMap(header, 'streak')
     fill = urwid.Filler(map1)
     map2 = urwid.AttrMap(fill, 'bg')
     loop = urwid.MainLoop(map2, (), unhandled_input=exit_on_quit)
     loop.run()
+
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+    except Exception as e:
+        print(str(e))
+        exit()
